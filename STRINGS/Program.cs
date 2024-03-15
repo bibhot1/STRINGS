e7ed028df4bb2545cf0db8e8aba1b79a9537db2d
Bibhot1 <bndazbvu.me12@gmail.com>
2024-03-16 07:34:52 +1300

    Add project files.

---
+++ STRINGS/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace STRINGS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Text based datatypes
+            string name = "Bibhot";
+            // defining a string needs coutasion marks
+            //defining a char needs single coutasion marks as it is a single letter.
+            char letter = 'a';
+
+        }
+    }
+}
